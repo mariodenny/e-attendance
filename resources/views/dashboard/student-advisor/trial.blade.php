8b42cde29bd13c9669fd86bdf64380a91ed47700
susanpayton <susanpayton789@gmail.com>
2025-10-24 11:48:10 +0700

    perubahan form

--- resources/views/dashboard/student-advisor/trial.blade.php
+++ resources/views/dashboard/student-advisor/trial.blade.php
@@ -6,8 +6,9 @@
 <div class="row">
     <div class="col m-2 p-2">
         <div class="form-group">
-            <form action="{{ route('student-advisor.trial.save') }}">
+            <form action="{{ route('student-advisor.trial.save') }}" method="POST">
                 <!--TODO :: Design form insert-->
+                <h1>Hello susan here</h1>
             </form>
         </div>
     </div>
