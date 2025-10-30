21c3094f57979ae875e306d81183e8921dfe6725
susanpayton <susanpayton789@gmail.com>
2025-10-30 11:07:00 +0700

    trial teacher

---
+++ resources/views/dashboard/teacher/trial.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.dashboard')
+
+@section('title', 'Teacher Dashboard')
+
+@section('welcome-title')
+    Welcome, {{ $teacherData->name }}
+@endsection
+
+@section('welcome-subtitle', 'Guide your students through their first coding experience')
+
+@section('content')
+
+
+
+
+@endsection
